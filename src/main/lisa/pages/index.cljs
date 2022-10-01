3199ddd2df17f1f67028a782857bcbe60cff1068
Christian Meter <cmeter@googlemail.com>
2022-10-02 00:02:42 +0200

    Add leistungen and profile

--- src/main/lisa/pages/index.cljs
+++ src/main/lisa/pages/index.cljs
@@ -1,24 +1,46 @@
 (ns lisa.pages.index
-  (:require ["@heroicons/react/24/outline" :refer [UserIcon UsersIcon UserGroupIcon FaceSmileIcon]]
-            [lisa.pages.base :refer [base]]))
+  (:require ["@heroicons/react/24/outline" :refer [FaceSmileIcon UserGroupIcon
+                                                   UserIcon UsersIcon]]
+            [lisa.pages.base :refer [base]]
+            [lisa.utils :as utils]))
 
 (defn- leistung [icon label]
   [:article.text-center
    [:> icon {:class "h-10 mx-auto"}]
    label])
 
+(defn- gray-line [num]
+  [:span.text-gray-400.bg-gray-400 (repeat num "a")])
+
+(defn- profil []
+  [:section {:class "w-3/4 mx-auto pt-10"}
+   [:h2.py-5 "Kurzprofil"]
+   [:div.flex.flex-row
+    [:div.pr-3
+     [:p [utils/lorem 42]]
+     [:p [utils/lorem 23]]
+     [:p [utils/lorem 32]]
+     [:p "---"]
+     [:p [utils/lorem 12]]]
+    [:img {:class "w-96"
+           :src "/img/womandog.webp"}]]])
+
+(defn- leistungen []
+  [:div.grid.grid-cols-4.gap-4.py-10
+   [leistung UserIcon "Einzelberatung"]
+   [leistung UsersIcon "Paarberatung"]
+   [leistung FaceSmileIcon "Familienberatung"]
+   [leistung UserGroupIcon "Teamberatung"]])
+
 (defn- index-page []
   [base {:page/title "Start"
          :page/description "TODO"}
    [:<>
     [:img {:src "/img/background.webp"}]
-    [:div.container.px-5
+    [:div.container.mx-auto
      [:h1 "Systemische Beratung Freund"]
-     [:div.grid.grid-cols-4.gap-4
-      [leistung UserIcon "Einzelberatung"]
-      [leistung UsersIcon "Paarberatung"]
-      [leistung FaceSmileIcon "Familienberatung"]
-      [leistung UserGroupIcon "Teamberatung"]]]]])
+     [leistungen]
+     [profil]]]])
 
 (defn index []
   [index-page])
--- src/main/lisa/utils.cljs
+++ src/main/lisa/utils.cljs
@@ -1,7 +1,13 @@
 (ns lisa.utils
-  (:require [com.fulcrologic.guardrails.core :refer [=> >defn ?]]
+  (:require [clojure.string :as str]
+            [com.fulcrologic.guardrails.core :refer [=> >defn ?]]
             [goog.string :refer [format]]
             [oops.core :refer [oset!]]))
+
+(defn lorem [n]
+  (let [lorem-ipsum (str/split "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+                               #" ")]
+    (str/join " " (take n lorem-ipsum))))
 
 (>defn set-website-title!
   "Set a document's website title."
